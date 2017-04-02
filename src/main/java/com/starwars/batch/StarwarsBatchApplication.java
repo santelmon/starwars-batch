7d675af5e1404ea2f84a310f1b8fbaa3091d827d
Cristobal Infantes <cinfantes@grupoasv.com>
2017-04-02 11:15:13 +0200

    repositorio inicializado

---
+++ src/main/java/com/starwars/batch/StarwarsBatchApplication.java
@@ -0,0 +1,12 @@
+package com.starwars.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarwarsBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarwarsBatchApplication.class, args);
+	}
+}

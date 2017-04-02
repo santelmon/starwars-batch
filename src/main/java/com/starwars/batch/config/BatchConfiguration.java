ccd60ad3d3a63a56f750a62ff51a5f82fcbcfba5
Cristobal Infantes <cinfantes@grupoasv.com>
2017-04-02 11:51:39 +0200

    hello world

---
+++ src/main/java/com/starwars/batch/config/BatchConfiguration.java
@@ -0,0 +1,33 @@
+package com.starwars.batch.config;
+
+import com.starwars.batch.tasklet.HellowWorldTasklet;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableBatchProcessing
+public class BatchConfiguration {
+
+  @Bean
+  public Step helloWorldStep(StepBuilderFactory stepBuilderFactory, HellowWorldTasklet hellowWorldTasklet){
+      return stepBuilderFactory
+        .get("helloWorldStep")
+        .tasklet(hellowWorldTasklet)
+        .build();
+  }
+
+  @Bean
+  public Job job(JobBuilderFactory jobBuilderFactory, Step helloWorldStep){
+    return jobBuilderFactory
+      .get("job")
+      .incrementer(new RunIdIncrementer())
+      .start(helloWorldStep)
+      .build();
+  }
+}

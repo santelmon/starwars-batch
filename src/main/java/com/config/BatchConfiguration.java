52e24e3f283fe92995d23bc21bc3c1596eed1613
Santi Mollá <santi.molla@yahoo.es>
2017-07-21 16:43:23 +0200

    He añadido el fichero de configuración para los procesos y un nuevo job.

---
+++ src/main/java/com/config/BatchConfiguration.java
@@ -0,0 +1,7 @@
+package com.config;
+
+/**
+ * Created by santel on 21/7/17.
+ */
+public class BatchConfiguration {
+}
---
+++ src/main/java/com/tasklet/HelloWorldTasklet.java
@@ -0,0 +1,7 @@
+package com.tasklet;
+
+/**
+ * Created by santel on 21/7/17.
+ */
+public class HelloWorldTasklet {
+}

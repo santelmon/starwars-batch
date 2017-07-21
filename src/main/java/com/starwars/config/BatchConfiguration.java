593e3b9b1ccf0b684115dd75a170a12c2362e604
Santi Mollá <santi.molla@yahoo.es>
2017-07-21 17:12:35 +0200

    He creado de nuevo el proyecto con los ficheros necesarios para hacer la primera prueba de los batch

---
+++ src/main/java/com/starwars/config/BatchConfiguration.java
@@ -0,0 +1,7 @@
+package com.starwars.config;
+
+/**
+ * Created by santel on 21/7/17.
+ */
+public class BatchConfiguration {
+}
---
+++ src/main/java/com/starwars/tasklet/HelloWorldTasklet.java
@@ -0,0 +1,7 @@
+package com.starwars.tasklet;
+
+/**
+ * Created by santel on 21/7/17.
+ */
+public class HelloWorldTasklet {
+}
